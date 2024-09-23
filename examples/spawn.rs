39a6eb6bc2975db45e7f777739c98b6c0aeaf18b
hardfist <yangjianzju@gmail.com>
2024-09-23 23:15:16 +0800

    chore: add io

--- examples/spawn.rs
+++ examples/spawn.rs
@@ -1,8 +1,10 @@
 use std::time::{Duration, Instant};
 
 use tokio::spawn;
-
-fn blocking_task(){
+fn blocking_cpu_task(){
+    
+}
+fn blocking_io_task(){
     std::thread::sleep(Duration::from_secs(1));
 }
 
@@ -10,13 +12,13 @@ fn blocking_task(){
 async fn main(){
     let start = Instant::now();
     let t1 =spawn(async {
-        blocking_task();
+        blocking_io_task();
     });
     let t2 = spawn(async {
-       blocking_task(); 
+       blocking_io_task(); 
     });
     let t3 = spawn(async {
-        blocking_task();
+        blocking_io_task();
     });
     let _t = tokio::join!(t1,t2,t3);
     let duration = start.elapsed().as_secs();
