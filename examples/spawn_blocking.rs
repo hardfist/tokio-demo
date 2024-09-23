46a591b50792c02d137cf72c35e75fe4c0acec47
hardfist <yangjianzju@gmail.com>
2024-09-23 23:19:39 +0800

    chore: add spawn_blocking

---
+++ examples/spawn_blocking.rs
@@ -0,0 +1,33 @@
+use std::time::{Duration, Instant};
+
+use tokio::{spawn, task::block_in_place};
+fn blocking_cpu_task(){
+    
+}
+fn blocking_io_task(){
+    std::thread::sleep(Duration::from_secs(1));
+}
+
+#[tokio::main(worker_threads =2)]
+async fn main(){
+    let start = Instant::now();
+    let t1 =spawn(async {
+        block_in_place(|| {
+            blocking_io_task();
+        })
+        
+    });
+    let t2 = spawn(async {
+        block_in_place(|| {
+            blocking_io_task();
+        })
+    });
+    let t3 = spawn(async {
+           block_in_place(|| {
+            blocking_io_task();
+        })
+    });
+    let _t = tokio::join!(t1,t2,t3);
+    let duration = start.elapsed().as_secs();
+    println!("secs: {duration}");
+}
